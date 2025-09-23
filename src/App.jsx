9408f7e1677cffd5f30526921fe7b502b8f6e388
Gautam Kumar <152634935+GoutamArya15@users.noreply.github.com>
2025-09-23 23:45:03 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Header from "./components/Header.jsx";
 import IntroSection from "./components/IIntroSection.jsx";
-import Footer from "./components/footer.jsx";
+import Footer from "./components/Footer.jsx";
 
 const App = () => {
   return (
